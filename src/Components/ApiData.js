4b3bcdde8f2b3107f16b8afb79b7308be032237c
Safeer Hayat <SAFHAYAT@Safeers-Air.home>
2018-02-17 22:38:06 +0000

    first commit

---
+++ src/Components/ApiData.js
@@ -0,0 +1,15 @@
+import igdb from "igdb-api-node";
+
+class ApiData extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+            <div></div>
+        )
+    }
+}
+ 
+export default ApiData;
